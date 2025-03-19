dde545d1a47d3f19d606dd91b6fca500ec4fbae0
Пучков Алексей <alexeyp542@mail.ru>
2025-03-19 10:51:24 +0300

    End front

---
+++ js/openModalWindow.js
@@ -0,0 +1,25 @@
+
+
+const bookingButtons = document.querySelectorAll(".booking-button");
+const body = document.querySelector("body");
+const modal = document.querySelector(".booking-modal-container");
+const closeModal = document.querySelector(".booking-modal__close");
+
+console.log(bookingButtons)
+
+for(let index = 0; index < bookingButtons.length; index++){
+    let bookingButton = bookingButtons[index];
+    bookingButton.addEventListener("click", function(){
+        openModal();
+    })
+}
+
+
+function openModal(){
+    modal.style.display = "block";
+}
+
+closeModal.addEventListener("click", () => {
+    modal.style.display = "none";
+})
+
