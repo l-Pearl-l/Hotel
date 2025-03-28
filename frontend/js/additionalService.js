0ef036a1c51249b369589ef55e7baf2cf83b47ae
Пучков Алексей <alexeyp542@mail.ru>
2025-03-28 19:02:28 +0300

    Add additional services

---
+++ frontend/js/additionalService.js
@@ -0,0 +1,58 @@
+
+
+const buttonOpeningAdditionalServices = document.querySelector(".open-additional-services");
+const additionalService = document.querySelector(".additional-services");
+const closeAdditionalServices = document.querySelector(".additional-service__close");
+const addtionalServicesButtons = document.querySelectorAll(".additional-service__card__button");
+const deleteServices = document.querySelectorAll(".additional-service__delete"); 
+const applyAdditionalServices = document.querySelector(".additional-service__apply");
+let groupAdditionalServices = [];
+
+buttonOpeningAdditionalServices.addEventListener("click", () => {
+    additionalService.style.display = "block";
+})
+
+closeAdditionalServices.addEventListener("click", () => {
+    additionalService.style.display = "none";
+})
+
+deleteServices.forEach(button => {
+    button.addEventListener("click", (event) => {
+        if(groupAdditionalServices.includes(event.target.classList[1])){
+            groupAdditionalServices = groupAdditionalServices.filter(service => service !== event.target.classList[1])   
+            event.target.style.opacity = 0.6;
+            event.target.disabled = true;
+        }
+        addtionalServicesButtons.forEach(button => {
+            if(!groupAdditionalServices.includes(button.classList[1])){
+                button.style.opacity = 1;
+                button.disabled = false;
+                button.textContent = "Добавить";
+                
+            }
+        })~
+    })
+})
+
+addtionalServicesButtons.forEach(button => {
+    button.addEventListener("click", (event) => {
+        if(!groupAdditionalServices.includes(event.target.classList[1])){
+            groupAdditionalServices.push(event.target.classList[1]);
+            event.target.style.opacity = 0.6;
+            event.target.disabled = true;
+            event.target.textContent = "Добавлено"
+        }
+        deleteServices.forEach(button => {
+            if(groupAdditionalServices.includes(button.classList[1])){
+                button.style.opacity = 1;
+                button.disabled = false;
+            }
+        })
+    })
+})
+
+
+
+applyAdditionalServices.addEventListener("click", () => {
+
+})
